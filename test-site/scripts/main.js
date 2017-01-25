15c6bd1c2a450b864a0e3166dfec0d883310f0f6
proerp5 <proerp5@gmail.com>
2017-01-25 18:22:44 +0530

    Commit for user interactivity js
    1/25/2017 commit 2

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -9,3 +9,19 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/test-image.png');
     }
 }
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
