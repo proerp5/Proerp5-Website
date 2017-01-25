29fac61d6d47419ae20c58c41ae792b105cc5b0d
proerp5 <proerp5@gmail.com>
2017-01-25 17:28:15 +0530

    Revert "js changes"
    This reverts commit 1fbd293bad4be1aa9326265904e610b6683568c4.

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -1,11 +1,16 @@
 var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
-	
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/test-image.png') {
+    if(mySrc === 'images/firefox-icon.png') {
+<<<<<<< HEAD
       myImage.setAttribute ('src','images/test-image-2.png');
     } else {
       myImage.setAttribute ('src','images/test-image.png');
+=======
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+>>>>>>> origin/Project-1
     }
 }
